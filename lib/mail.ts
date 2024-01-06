d9e916e6c0a49f9f010336b49bb1a76aa742680b
chandrabhan-singh-1 <chandrabhansingh813@gmail.com>
2024-01-06 21:43:39 +0530

    2: Next Public APP Url Updated

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,9 +1,10 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
@@ -12,8 +13,6 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     html: `<p>Click <a href="${confirmLink}">here</a> to verify your Email at Next-Auth-JS.</p>\n<p>This link will expire after 1 hour.</p>`,
   });
 };
-
-const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
